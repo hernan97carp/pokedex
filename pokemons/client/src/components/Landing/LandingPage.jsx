cd501fa8f61ad3ca73ff8bcb556753145c4ff4f4
hernan97carp <hernan97carp@hotmail.com>
2023-05-13 16:15:57 -0300

    commit

--- pokemons/client/src/components/Landing/LandingPage.jsx
+++ pokemons/client/src/components/Landing/LandingPage.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import "./landingPage.css"
-import welcomeImage from "../../images/pikachu.jpeg"
 export default function LandingPage (){
 
 
@@ -12,10 +11,6 @@ return (
  
      <h3 className="landingText">Hello Everyone</h3>
     
-    
-   <img className='landingImage' src={welcomeImage}/>
-
- 
    <Link to="/home">
    <button className="ov-btn-grow-box">ready? So, let's get started.</button>
   </Link>
