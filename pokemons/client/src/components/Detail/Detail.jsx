f04ebdb18b7fe69e7a9d6005024f038431a17173
hernan97carp <hernan97carp@hotmail.com>
2023-05-17 17:23:21 -0300

    commit

--- pokemons/client/src/components/Detail/Detail.jsx
+++ pokemons/client/src/components/Detail/Detail.jsx
@@ -5,7 +5,7 @@ import { getDetail } from "../../redux/Action/action";
 import "./detail.css";
 import random from "../../images/random.png";
 import loading from "../../images/loang.gif";
-import style from "./Detail.module.css";
+// import style from "./Detail.module.css";
 
 import heartImg from "../../images/cards/health.png";
 import attackImg from "../../images/cards/sword.png";
@@ -75,96 +75,146 @@ function Detail(props) {
             </span>
           </div>
 
-          <div className="progress">
-            <span className="hp">{myPokemon[0].hp}%</span>
+          <div className="progress-bar red stripes shine">
+            <span
+              style={{
+                width: myPokemon[0].hp > 100 ? "100%" : myPokemon[0].hp + "%",
+              }}
+              className="hp"
+            >
+              {myPokemon[0].hp > 100 ? "100%" : myPokemon[0].hp + "%"}
+            </span>
           </div>
         </div>
 
         {/* attack */}
         <div className="bar">
-          <div className="infoo">
+          <div className="info">
             <span>
               <img src={attackImg} className="imagenes" />
               Attack
             </span>
           </div>
-          <div className="progress">
-            <span>{myPokemon[0].attack}%</span>
+          <div className="progress-bar black stripes shine">
+            <span
+              style={{
+                width:
+                  myPokemon[0].attack > 200
+                    ? "200%"
+                    : myPokemon[0].attack + "%",
+              }}
+              per={`${myPokemon[0].attack}`}
+              className="attack"
+            >
+              {myPokemon[0].attack > 100 ? "100%" : myPokemon[0].attack + "%"}
+            </span>
           </div>
         </div>
 
         {/* defense*/}
         <div className="bar">
-          <div className="infoo">
+          <div className="info">
             <span>
               <img src={shield} className="imagenes" />
               Defense
             </span>
           </div>
-          <div className="progress">
-            <span>{myPokemon[0].defense}%</span>
+          <div className="progress-bar blue stripes shine">
+            <span
+              style={{
+                width:
+                  myPokemon[0].defense > 100
+                    ? "100%"
+                    : myPokemon[0].defense + "%",
+              }}
+              per={`${myPokemon[0].defense}`}
+              className="defense"
+            >
+              {myPokemon[0].defense > 100 ? "100%" : myPokemon[0].defense + "%"}
+            </span>
           </div>
         </div>
 
         {/*speed */}
         <div className="bar">
-          <div className="infoo">
+          <div className="info">
             <span>
               <img src={speed} className="imagenes" />
               Speed
             </span>
           </div>
-          <div className="progress">
-            <span>{myPokemon[0].speed}%</span>
+          <div className="progress-bar orange stripes shine">
+            <span
+              style={{
+                width:
+                  myPokemon[0].speed > 100 ? "100%" : myPokemon[0].speed + "%",
+              }}
+              className="speed"
+            >
+              {myPokemon[0].speed > 100 ? "100%" : myPokemon[0].speed + "%"}
+            </span>
           </div>
         </div>
 
         {/*happy*/}
 
         <div className="bar">
-          <div className="infoo">
+          <div className="info">
             <span>
               <img src={happy} className="imagenes" />
               Happy
             </span>
           </div>
-          <div className="progress">
-            <span>{myPokemon[0].happiness}%</span>
+          <div className="progress-bar green stripes shine">
+            <span
+              style={{
+                width:
+                  myPokemon[0].happiness > 200
+                    ? "200%"
+                    : myPokemon[0].happiness + "%",
+              }}
+              per={`${myPokemon[0].happiness}`}
+              className="happiness"
+            >
+              {myPokemon[0].happiness > 100
+                ? "100%"
+                : myPokemon[0].happiness + "%"}
+            </span>
           </div>
         </div>
 
         {/* weight */}
 
         <div className="bar">
-          <div className="infoo">
+          <div className="info">
             <span>
               <img src={weight} className="imagenes" />
               Weight
             </span>
           </div>
           <div className="heightAndWeight">
-            <span>{myPokemon[0].weight}kg</span>
+            <span>{myPokemon[0].weight / 10}kg</span>
           </div>
         </div>
 
         {/*height*/}
 
         <div className="bar">
-          <div className="infoo">
+          <div className="info">
             <span>
               <img src={height} className="imagenes" />
               Height
             </span>
           </div>
           <div className="heightAndWeight">
-            <span>{myPokemon[0].height}m</span>
+            <span>{myPokemon[0].height / 10}m</span>
           </div>
         </div>
 
         {/* description */}
 
         <div className="bar">
-          <div className="infoo">
+          <div className="info">
             <span>
               <img src={pokedex} className="imagenes" />
               Description:
@@ -176,6 +226,11 @@ function Detail(props) {
         </div>
 
         {/* div de stats  abajo*/}
+      </div>
+      <div className="buttonConteiner">
+        <Link to="/home">
+          <button className="buttonDetail">Back</button>
+        </Link>
       </div>
     </div>
   );
--- pokemons/client/src/components/Home/Home.jsx
+++ pokemons/client/src/components/Home/Home.jsx
@@ -18,181 +18,159 @@ import notFound from "../../images/random.png";
 import pika from "../../images/reload1.webp";
 import NavSearch from "../NavSearch/NavSearch";
 import loading from "../../images/loang.gif";
-import "./home.css"
+import "./home.css";
 export default function Home() {
-
-
-
-
   const dispatch = useDispatch();
   const allPokemons = useSelector((state) => state.pokemons);
   const types = useSelector((state) => state.types);
-//gif loading
+  //gif loading
   const [pokLoaded, setPokLoaded] = useState(allPokemons.length ? true : false);
   //setear cambio de orden
   const [orden, setOrden] = useState("");
 
   //numero de la pagina empieza en 1
   const [currentPage, setCurrentPage] = useState(1);
+
   //pokemones por pagina
   const [pokemonsPerPage, setPokemonsPerPage] = useState(12);
   const indexOfLastPokemon = currentPage * pokemonsPerPage;
   const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
-  const currentPokemons = allPokemons.slice(indexOfFirstPokemon, indexOfLastPokemon)
+  const currentPokemons = allPokemons.slice(
+    indexOfFirstPokemon,
+    indexOfLastPokemon
+  );
 
   const paginado = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
   useEffect(() => {
-    dispatch(getTypes())
-    if(!pokLoaded){
-      dispatch(getPokemons())
-    } ;
-  }, [pokLoaded,dispatch]);
+    dispatch(getTypes());
+    if (!pokLoaded) {
+      dispatch(getPokemons());
+    }
+  }, [pokLoaded, dispatch]);
 
-useEffect(()=>{
-//  Numero de la pagina comienza en 1 me trae las carts de la pagina uno
-setCurrentPage(1)
+  useEffect(() => {
+    //  Numero de la pagina comienza en 1 me trae las carts de la pagina uno
+    setCurrentPage(1);
+  }, [allPokemons.length, setCurrentPage]);
 
-},[allPokemons.length,setCurrentPage])
+  //Button reload
 
+  function handlerReload(evento) {
+    evento.preventDefault();
+    dispatch(reloadPokemons());
+  }
 
-//Button reload
+  // FILTRAR Y ORDENAR
+  function handleOrder(evento) {
+    evento.preventDefault();
+    dispatch(orderByNameOrStrengh(evento.target.value));
+    setCurrentPage(currentPage);
+    //setear para que capte el cambio
+    setOrden(evento.target.value);
+  }
 
-function handlerReload(evento){
-  evento.preventDefault()
-dispatch(reloadPokemons())
+  function handleFilterCreated(evento) {
+    dispatch(filterCreated(evento.target.value));
+  }
 
-}
-
-
-
-
-// FILTRAR Y ORDENAR
-function handleOrder(evento){
-  evento.preventDefault();
-  dispatch(orderByNameOrStrengh(evento.target.value))
-  setCurrentPage(currentPage);
-  //setear para que capte el cambio
-  setOrden(evento.target.value)
-
-}
-
-function handleFilterCreated(evento){
-  dispatch(filterCreated(evento.target.value));
-
-}
-
-function handleFilterByType(evento){
-  evento.preventDefault();
- dispatch(filterPokemonsByType(evento.target.value))
-
-}
-
+  function handleFilterByType(evento) {
+    evento.preventDefault();
+    dispatch(filterPokemonsByType(evento.target.value));
+  }
 
   return (
-<>
+    <>
+      <div className="home">
+        {/*BARRA NAVEGACION */}
+        <NavBar />
+        {/*BARRA DE BUSQUEDA */}
+        <NavSearch />
 
-    
-    <div className="home">
-    {/*BARRA NAVEGACION */}
-      <NavBar />
-      {/*BARRA DE BUSQUEDA */}
-      <NavSearch />
+        <button onClick={(evento) => handlerReload(evento)} className="reload">
+          <img src={pika} className="pika" alt="imagen pikachu"></img>Reload
+        </button>
 
+        {/* FILTRAR Y ORDENAR */}
 
-<button onClick={(evento)=>handlerReload(evento)} className="reload"
-><img src={pika} className="pika" alt="imagen pikachu"></img>Reload</button>
+        <div className="selects">
+          <select onChange={(evento) => handleOrder(evento)}>
+            <option value="normal">Normal</option>
+            <option value="asc">A - Z</option>
+            <option value="HAttack">Highest Attack</option>
+            <option value="LAttack">Lowest Attack</option>
+          </select>
 
+          <select onChange={(evento) => handleFilterCreated(evento)}>
+            <option value="All">All</option>
+            <option value="api">Api</option>
+            <option value="created">Created</option>
+          </select>
+          <select onChange={(evento) => handleFilterByType(evento)}>
+            <option value="All">All types</option>
+            {types.map((type) => (
+              <option value={type.name} key={type.name}>
+                {type.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-{/* FILTRAR Y ORDENAR */}
+        {/* FIN DE  FILTRAR Y ORDENAR */}
 
-<div className="selects">
- <select onChange={evento => handleOrder(evento)}>
-<option value="normal">Normal</option>
-<option value="asc">A - Z</option>
-<option value="HAttack">Highest Attack</option>
-<option value="LAttack">Lowest Attack</option>
- </select>
+        {/* PAGINADO       */}
+        <Paginado
+          //POKEMONES POR PAGINA QUE VA A TENER ...12
+          pokemonsPerPage={pokemonsPerPage}
+          allPokemons={allPokemons.length}
+          paginado={paginado}
+          page={currentPage}
+        />
+        {/*SHOW CARTS */}
 
- <select onChange={evento => handleFilterCreated(evento)}>
-<option value="All">All</option>
-<option value="api">Api</option>
-<option value="created">Created</option>
- </select>
- <select onChange={evento => handleFilterByType(evento)}>
- <option value="All">All types</option>
-                    {
-                        types.map( type => (
-                            <option value={type.name} key={type.name}>{type.name}</option>
-                        ))
-                    }
- </select>
-</div>
-
-{/* FIN DE  FILTRAR Y ORDENAR */}
-
-
-
-    {/* PAGINADO       */}
-    <Paginado
-                //POKEMONES POR PAGINA QUE VA A TENER ...12
-                pokemonsPerPage={pokemonsPerPage}
-                
-                allPokemons = {allPokemons.length}
-                paginado={paginado}
-               page={currentPage}
-            />
-      {/*SHOW CARTS */}
-
-      {
-           currentPokemons.length ? 
-           typeof currentPokemons[0] === 'object' ?
-      currentPokemons.map((el) => {
-        return (
-          <div>
-            <Link
-              to={"/home/" + el.id}
-              style={{ textDecoration: "none" }}
-              key={el.id}
-            >
-              <Card
-                name={el.name}
-                types={el.types}
-                image={el.img ? el.img : notFound}
-                id={el.id}
-                weight={el.weight}
-                height={el.height}
+        {currentPokemons.length ? (
+          typeof currentPokemons[0] === "object" ? (
+            currentPokemons.map((el) => {
+              return (
+                <div>
+                  <Link
+                    to={"/home/" + el.id}
+                    style={{ textDecoration: "none" }}
+                    key={el.id}
+                  >
+                    <Card
+                      name={el.name}
+                      types={el.types}
+                      image={el.img ? el.img : notFound}
+                      id={el.id}
+                      weight={el.weight}
+                      height={el.height}
+                    />
+                  </Link>
+                </div>
+              );
+            })
+          ) : (
+            <div className="notfound">
+              <img
+                src="images/notfound.png"
+                alt="Pokemon not found"
+                width="100px"
               />
-            </Link>
+              <span>{currentPokemons[0]} not found</span>
+            </div>
+          )
+        ) : (
+          <div className="loadingConteiner">
+            <img src={loading} className="loading"></img>
           </div>
-        )
-      }) :
-
-      <div className="notfound">
-      <img src='images/notfound.png'alt="Pokemon not found" width='100px'/>
-      <span>{currentPokemons[0]} not found</span>
-  </div>
-
-   
-    :
-    <div className="loadingConteiner">
-      <img src={loading} className="loading"></img>
-    </div>
-    }
-                
-
-
-
-    </div>
-</>
-
+        )}
+      </div>
+    </>
   );
-
 }
-
-
 
 // import"./home.css"
 // import NavBar from "../NavBar/NavBar"
