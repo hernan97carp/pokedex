3abed5baf453f7f7531755dc9a0f38e1ee1c7aad
hernan97carp <hernan97carp@hotmail.com>
2023-05-19 15:34:04 -0300

    commit

--- pokemons/client/src/components/createPokemon/CreatePokemon.jsx
+++ pokemons/client/src/components/createPokemon/CreatePokemon.jsx
@@ -144,9 +144,19 @@ export default function PokemonCreate() {
 
   return (
     <div className={style.pagina}>
-      <Link to="/home" className={style.return}>
-        <button className={style.retbtn}>Return home</button>
-      </Link>
+      <div className={style.returnbtn}>
+        <Link to="/home">
+          <button className={style.retbtn}>Return home</button>
+        </Link>
+      </div>
+      <img
+        src={Oak}
+        alt="Profesor Oak"
+        height="230px"
+        width="200px"
+        className={style.img}
+      />
+
       <div className={style.container}>
         <div className={style.header}>
           <h2>Create your pokemon!</h2>
@@ -430,7 +440,7 @@ export default function PokemonCreate() {
                 style={{
                   display: "flex",
                   justifyContent: "flex-start",
-                  fontFamily: "Poppins",
+                  fontFamily: "Montserrat",
                 }}
               >
                 Choose up to 2 Pokemon types
@@ -496,100 +506,6 @@ export default function PokemonCreate() {
           </section>
         </form>
       </div>
-      <img src={Oak} alt="Profesor Oak" height="500px" className={style.img} />
     </div>
   );
 }
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import { useHistory } from "react-router-use-history";
-// import profesorImg from "../../images/oak2.gif";
-// import { useDispatch, useSelector } from "react-redux";
-
-// export default function CreatePokemon() {
-//   const dispatch = useDispatch();
-//   const types = useSelector((state) => state.types);
-//   const pokemons = useSelector((state) =>
-//     state.pokemons.map((poke) => poke.name)
-//   );
-//   const history = useHistory();
-//   const [inputs, setInputs] = useState({
-//     name: "",
-//     hp: "",
-//     attack: "",
-//     defense: "",
-//     speed: "",
-//     weight: "",
-//     height: "",
-//     types: [],
-//   });
-
-//   return (
-//     <>
-//       <div className="conteinerCreate">
-//         <Link to="/home">
-//           <button className="buttonBack">Back</button>
-//         </Link>
-//         <div className="page">
-//           <div className="header">
-//             <h2>Create your Pokemon</h2>
-//           </div>
-//           <form className="formPage">
-//             <section>
-//               <div className="formDive">
-//                 <label>name</label>
-//                 <input type="text" name="name" autoComplete="off" />
-//               </div>
-
-//               <div className="formDive">
-//                 <label>hp</label>
-//                 <input type="number" name="hp" autoComplete="off" />
-//               </div>
-
-//               <div className="formDive">
-//                 <label>attack</label>
-//                 <input type="number" name="attack" autoComplete="off" />
-//               </div>
-
-//               <div className="formDive">
-//                 <label>defense</label>
-//                 <input type="number" name="defense" autoComplete="off" />
-//               </div>
-
-//               <div className="formDive">
-//                 <label>speed</label>
-//                 <input type="number" name="speed" autoComplete="off" />
-//               </div>
-
-//               <div className="formDive">
-//                 <label>weight</label>
-//                 <input type="number" name="weight" autoComplete="off" />
-//               </div>
-
-//               <div className="formDive">
-//                 <label>height</label>
-//                 <input type="number" name="height" autoComplete="off" />
-//               </div>
-
-//               <button className="buttonNext">Next</button>
-//             </section>
-
-//             <section>
-//               <div>
-//                 <span className="choosetypes">Choose two Pokemon Types</span>
-//                 <div className="conteinerTypes">{}</div>
-
-//                 <div className="btnPreviusCreate">
-//                   <button className="previus">Previus</button>
-//                   <button className="create">Create</button>
-//                 </div>
-//               </div>
-//             </section>
-//           </form>
-//         </div>
-//         <img src={profesorImg} alt="profesor img" height="400px"></img>
-//       </div>
-//     </>
-//   );
-// }
