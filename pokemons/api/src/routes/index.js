3cf5c21afb5d1d7de6958ff8efcbfe2b6116f598
hernan97carp <hernan97carp@hotmail.com>
2023-05-19 12:49:03 -0300

    commit

--- pokemons/api/src/routes/index.js
+++ pokemons/api/src/routes/index.js
@@ -230,36 +230,53 @@ router.get("/types", async (req, res) => {
 });
 
 router.post("/pokemons", async (req, res) => {
-  const {
-    name,
-    types,
-    hp,
-    attack,
-    defense,
-    speed,
-    height,
-    weight,
-    img,
-    createdInDb,
-  } = req.body;
+  try {
+    const {
+      name,
+      types,
+      hp,
+      attack,
+      defense,
+      speed,
+      height,
+      weight,
+      img,
+      createdInDb,
+    } = req.body;
 
-  const pokemonCreated = await Pokemon.create({
-    name,
-    hp,
-    attack,
-    defense,
-    speed,
-    height,
-    weight,
-    img,
-    createdInDb: true,
-  });
+    if (
+      !name &&
+      !types &&
+      !hp &&
+      !attack &&
+      !defense &&
+      !speed &&
+      !height &&
+      !weight &&
+      !img
+    )
+      throw Error("complete everything");
 
-  const pokemonTypes = await Type.findAll({
-    where: { name: types },
-  });
-  pokemonCreated.addType(pokemonTypes);
-  return res.send("Pokemon created successfuly");
+    const pokemonCreated = await Pokemon.create({
+      name,
+      hp,
+      attack,
+      defense,
+      speed,
+      height,
+      weight,
+      img,
+      createdInDb: true,
+    });
+
+    const pokemonTypes = await Type.findAll({
+      where: { name: types },
+    });
+    pokemonCreated.addType(pokemonTypes);
+    return res.send("Pokemon created successfuly");
+  } catch (error) {
+    res.status(404).send("not created");
+  }
 });
 
 router.get("/pokemons/:idPokemon", async (req, res) => {
