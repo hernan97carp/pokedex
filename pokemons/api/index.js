dd00e0978af74c4c9b3564e2c7cc65e1ede6d68f
hernan97carp <hernan97carp@hotmail.com>
2023-06-25 14:11:26 -0300

    commit

--- pokemons/api/index.js
+++ pokemons/api/index.js
@@ -1,14 +1,11 @@
-
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-
-
-
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
 // Syncing all the models at once.
 // force : true will drop the table if it already exists VALOR POR DEFECTO
 // force : false will not drop the table if it already exists
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('Server listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log(`Server listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
